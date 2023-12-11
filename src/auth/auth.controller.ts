db2595cdd52eb2510d1a875b35c618fa5a2c86d4
Ihor Karen <karenihor@gmail.com>
2023-12-11 18:52:08 +0200

    AuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
 import {
+  Request,
   Body,
   Controller,
+  Get,
   Post,
   HttpException,
   HttpCode,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 export class UsersController {
@@ -67,5 +71,16 @@ export class UsersController {
     }
 
     return { name: user.name, email, token: user.token };
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('current')
+  @HttpCode(200)
+  async getCurrent(@Request() req) {
+    const { email, name } = req.user;
+
+    const { token } = await this.usersService.findEmail(email);
+
+    return { name, email, token };
   }
 }
---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { Model } from 'mongoose';
+import { User } from 'src/auth/schemas/user.schema';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request.headers;
+    const [bearer, token] = authorization.split(' ') ?? [];
+
+    if (bearer !== 'Bearer' || !token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const { _id } = await this.jwtService.verifyAsync(token);
+      const user = await this.userModel.findById(_id);
+
+      request['user'] = user;
+      return true;
+    } catch {
+      throw new UnauthorizedException();
+    }
+  }
+}
